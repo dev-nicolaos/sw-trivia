fd82e51e3be8bafec562acc81f37750385c76d87
dev-nicolaos <dev@nicolaosskimas.com>
2019-07-03 22:48:31 -0700

    correctAnswer can be last in getPossibleAnswers

--- src/helpers/get_possible_answers.ts
+++ src/helpers/get_possible_answers.ts
@@ -3,7 +3,7 @@ import { QUESTION } from '../interfaces/mod.ts';
 export function getPossibleAnswers(question: QUESTION): string[] {
   const { wrongAnswers, correctAnswer } = question;
   const possibleAnswers = [...wrongAnswers];
-  const insertIndex = Math.floor(Math.random() * possibleAnswers.length);
+  const insertIndex = Math.floor(Math.random() * (possibleAnswers.length + 1));
   possibleAnswers.splice(insertIndex, 0, correctAnswer);
   return possibleAnswers;
 }
