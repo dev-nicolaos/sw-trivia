3150bf3f0e47f5aad9b0e9fd8fd023172c979db9
dev-nicolaos <dev@nicolaosskimas.com>
2019-07-04 01:56:01 -0700

    fix forgotten file extension in import statement

--- src/helpers/mod.ts
+++ src/helpers/mod.ts
@@ -1,4 +1,4 @@
 export { askRandomQuestion } from './ask_random_question.ts';
 export { EOL } from './eol.ts';
 export { getInput } from './get_input.ts';
-export { getPossibleAnswers } from './get_possible_answers';
+export { getPossibleAnswers } from './get_possible_answers.ts';
