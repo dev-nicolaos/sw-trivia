78021692d58923e3246849cfc35f026fbf5a9d52
dev-nicolaos <dev@nicolaosskimas.com>
2019-07-03 17:00:25 -0700

    first crack at asking a question

---
+++ src/helpers/ask_random_question.ts
@@ -0,0 +1,23 @@
+import { QUESTION } from '../interfaces/mod.ts';
+import { getPossibleAnswers } from './get_possible_answers.ts';
+
+export function askRandomQuestion(trivia: QUESTION[]) {
+  const selectedIndex = Math.floor(Math.random() * trivia.length);
+  const selectedTrivia = trivia[selectedIndex];
+  const possibleAnswers = getPossibleAnswers(selectedTrivia);
+
+  console.log(selectedTrivia.question);
+  possibleAnswers.forEach((answer, i) => {
+    console.log((i + 1) + ')', answer);
+  });
+
+  // Listen for response
+
+  /* if (possibleAnswers[(+response) - 1] === selectedTrivia.correctAnswer) {
+    console.log('Correct ✔');
+  } else {
+    console.log('Incorrect 😢');
+    console.log('The correct answer is', selectedTrivia.correctAnswer);
+  }
+  console.log('Source:', selectedTrivia.source.name); */
+}
--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,7 @@
 import { parse } from './deps.ts';
 import showStats from './stats/stats.ts';
+import { askRandomQuestion } from './helpers/ask_random_question.ts';
+import trivia from './trivia/mod.ts';
 
 const { args } = Deno;
 
@@ -9,5 +11,5 @@ const statsFlag = parse(args).stats;
 if (statsFlag) {
   showStats();
 } else {
-  // TODO: ask a question
+  askRandomQuestion(trivia);
 }
