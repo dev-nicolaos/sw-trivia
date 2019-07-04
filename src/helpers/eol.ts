3e43c7e037e328a7c4b7d3c5f5dbbdf4c98e7125
dev-nicolaos <dev@nicolaosskimas.com>
2019-07-04 01:34:00 -0700

    add separate eol helper

---
+++ src/helpers/eol.ts
@@ -0,0 +1,1 @@
+export const EOL = Deno.platform.os === 'win' ? '\r\n' : '\n';
--- src/helpers/get_input.ts
+++ src/helpers/get_input.ts
@@ -1,3 +1,5 @@
+import { EOL } from './eol.ts';
+
 export async function getInput(prompt: string = ''): Promise<string> {
   if (prompt) {
     console.log(prompt);
@@ -6,9 +8,8 @@ export async function getInput(prompt: string = ''): Promise<string> {
   const { stdin, platform } = Deno;
   const decoder = new TextDecoder();
   const byteArray = new Uint8Array(1024);
-  const EOLCHAR = platform.os === 'win' ? '\r\n' : '\n';
 
   await stdin.read(byteArray);
   const line = decoder.decode(byteArray);
-  return line.substring(0, line.indexOf(EOLCHAR));
+  return line.substring(0, line.indexOf(EOL));
 }
