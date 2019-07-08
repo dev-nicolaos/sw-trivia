f3477f9c889df898bf3e220803d2f96dc13d743c
dev-nicolaos <dev@nicolaosskimas.com>
2019-07-07 23:10:00 -0700

    add trivia for esb

--- src/trivia/films/the_empire_strikes_back.ts
+++ src/trivia/films/the_empire_strikes_back.ts
@@ -3,14 +3,170 @@ import { TRIVIA } from '../../interfaces/mod.ts';
 const trivia: TRIVIA[] = [
   {
     question: `Prior to Disney's purchase of Lucasfilm, which Star Wars film was the only one not to show Tatooine?`,
-    correctAnswer: `Episode V: Empire Strikes Back`,
+    correctAnswer: `Episode V: The Empire Strikes Back`,
     wrongAnswers: [
       `Episode III: Revenge of the Sith`,
       `Episode II: Attack of the Clones`,
       `Episode VI: Return of the Jedi`,
     ],
     source: {
-      name: `Episode V: Empire Strikes Back`,
+      name: `Episode V: The Empire Strikes Back`,
+      mediaType: `Film`,
+    },
+  },
+  {
+    question: `What creatures did the rebels ride on Hoth?`,
+    correctAnswer: `Tauntauns`,
+    wrongAnswers: [`Wampas`, `Dewbacks`, `Rontos`],
+    source: {
+      name: `Episode V: The Empire Strikes Back`,
+      mediaType: `Film`,
+    },
+  },
+  {
+    question: `What did Luke originally think the probe droid that crashed onto Hoth was?`,
+    correctAnswer: `A meteorite`,
+    wrongAnswers: [`A ship`, `An orbital strike`, `An avalanche`],
+    source: {
+      name: `Episode V: The Empire Strikes Back`,
+      mediaType: `Film`,
+    },
+  },
+  {
+    question: `Who was in command of Echo Base?`,
+    correctAnswer: `General Riekkan`,
+    wrongAnswers: [`General Madiene`, `General Veers`, `General Kenobi`],
+    source: {
+      name: `Episode V: The Empire Strikes Back`,
+      mediaType: `Film`,
+    },
+  },
+  {
+    question: `Who led the ground assult on Echo Base?`,
+    correctAnswer: `General Veers`,
+    wrongAnswers: [`General Grievious`, `General Krell`, `General Hux`],
+    source: {
+      name: `Episode V: The Empire Strikes Back`,
+      mediaType: `Film`,
+    },
+  },
+  {
+    question: `Finish the quote: "I'd just as soon kiss a _____"`,
+    correctAnswer: `wookie!`,
+    wrongAnswers: [`rodian!`, `dianoga!`, `wampa!`],
+    source: {
+      name: `Episode V: The Empire Strikes Back`,
+      mediaType: `Film`,
+    },
+  },
+  {
+    question: `What rank did Luke Skywalker hold at the Battle of Hoth?`,
+    correctAnswer: `Commander`,
+    wrongAnswers: [`Deck Officer`, `Captain`, `General`],
+    source: {
+      name: `Episode V: The Empire Strikes Back`,
+      mediaType: `Film`,
+    },
+  },
+  {
+    question: `Which planet did Obi-Wan tell Luke to find Yoda on?`,
+    correctAnswer: `Dagobah`,
+    wrongAnswers: [`Kashyyyk`, `Coruscant`, `Endor`],
+    source: {
+      name: `Episode V: The Empire Strikes Back`,
+      mediaType: `Film`,
+    },
+  },
+  {
+    question: `According to R2D2, what are the odds of surviving a night on Hoth outside?`,
+    correctAnswer: `725 to 1`,
+    wrongAnswers: [`3,750 to 1`, `250 to 1`, `10,000 to 1`],
+    source: {
+      name: `Episode V: The Empire Strikes Back`,
+      mediaType: `Film`,
+    },
+  },
+  {
+    question: `How did Han keep Luke warm while he setup the shelter on Hoth?`,
+    correctAnswer: `Stuffed him inside a dead Tauntaun`,
+    wrongAnswers: [
+      `Started a fire`,
+      `Activated a portable heater`,
+      `Wrapped him in a thermal blanket`,
+    ],
+    source: {
+      name: `Episode V: The Empire Strikes Back`,
+      mediaType: `Film`,
+    },
+  },
+  {
+    question: `How many AT-ATs did Rogue squadron take down with harpoons and tow cables?`,
+    correctAnswer: `1`,
+    wrongAnswers: [`3`, `2`, `4`],
+    source: {
+      name: `Episode V: The Empire Strikes Back`,
+      mediaType: `Film`,
+    },
+  },
+  {
+    question: `What sector did the rebels detect a fleet of Star Destroyers emerging from above Hoth?`,
+    correctAnswer: `Sector 4`,
+    wrongAnswers: [`Sector 9`, `Sector 7`, `Sector 2`],
+    source: {
+      name: `Episode V: The Empire Strikes Back`,
+      mediaType: `Film`,
+    },
+  },
+  {
+    question: `Which Imperial did Darth Vader choke just before the Battle of Hoth?`,
+    correctAnswer: `Admiral Ozzle`,
+    wrongAnswers: [`Captain Piett`, `Captain Needa`, `Moff Jerijodd`],
+    source: {
+      name: `Episode V: The Empire Strikes Back`,
+      mediaType: `Film`,
+    },
+  },
+  {
+    question: `How many rebel fighters escorted each transport off Hoth?`,
+    correctAnswer: `2`,
+    wrongAnswers: [`4`, `1`, `3`],
+    source: {
+      name: `Episode V: The Empire Strikes Back`,
+      mediaType: `Film`,
+    },
+  },
+  {
+    question: `Finish the quote "Hurry up golden rod! You're gonna be _____"`,
+    correctAnswer: `a permanent resident!`,
+    wrongAnswers: [
+      `a pile of scrap!`,
+      `left behind!`,
+      `imperial target practice!`,
+    ],
+    source: {
+      name: `Episode V: The Empire Strikes Back`,
+      mediaType: `Film`,
+    },
+  },
+  {
+    question: `How many TIE fighters pursued the Millenium Falcon into the astroid field?`,
+    correctAnswer: `4`,
+    wrongAnswers: [`6`, `2`, `3`],
+    source: {
+      name: `Episode V: The Empire Strikes Back`,
+      mediaType: `Film`,
+    },
+  },
+  {
+    question: `How did Luke initially describe Dagobah?`,
+    correctAnswer: `Like something out of a dream`,
+    wrongAnswers: [
+      `Strong with the force`,
+      `One big swamp`,
+      `A forgotten planet`,
+    ],
+    source: {
+      name: `Episode V: The Empire Strikes Back`,
       mediaType: `Film`,
     },
   },
