d50eb2721b057ba026083404fec56201c9b50321
dev-nicolaos <dev@nicolaosskimas.com>
2019-07-18 23:22:20 -0700

    bump deno version

--- src/deps.ts
+++ src/deps.ts
@@ -5,5 +5,5 @@ export {
   bgYellow,
   black,
   white,
-} from 'https://deno.land/std@v0.11.0/colors/mod.ts';
-export { parse } from 'https://deno.land/std@v0.11.0/flags/mod.ts';
+} from 'https://deno.land/std@v0.12.0/colors/mod.ts';
+export { parse } from 'https://deno.land/std@v0.12.0/flags/mod.ts';
