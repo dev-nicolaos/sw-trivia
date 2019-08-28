d06c3740c347dbbf75c691410034392b7c770ae3
dev-nicolaos <dev@nicolaosskimas.com>
2019-08-27 18:05:50 -0700

    add more questions for Thrawn

--- src/trivia/books/thrawn.ts
+++ src/trivia/books/thrawn.ts
@@ -10,6 +10,35 @@ const trivia: TRIVIA[] = [
     wrongAnswers: ['Basic', 'Huttesse', 'Shyriiwook'],
     source,
   },
+  {
+    question: 'Why did Thrawn ask Palpatine to assign Eli Vanto to him?',
+    correctAnswer: 'To learn what Vanto knew of the Chiss',
+    wrongAnswers: [
+      'To translate for him',
+      "Because of Vanto's tactical skills",
+      'To teach him the politcal hierachy of the Empire',
+    ],
+    source,
+  },
+  {
+    question: 'Which district of Coruscant was The Pinnacle located in?',
+    correctAnswer: 'The Federal District',
+    wrongAnswers: ['The Uscru District', 'The Works', 'CoCo Town'],
+    source,
+  },
+  {
+    question: 'From which rank was Eli Vanto promoted to Lieutenant Commander?',
+    correctAnswer: 'Ensign',
+    wrongAnswers: ['Lieutenant', 'Captain', 'Corporal'],
+    source,
+  },
+  {
+    question:
+      'Who was the first person Thrawn tried to recruit into Chiss service while he was in the Imperial Navy?',
+    correctAnswer: 'Nevil Cygni',
+    wrongAnswers: ['Eli Vanto', 'Brierly Ronan', 'Wullf Yularen'],
+    source,
+  },
 ];
 
 export default trivia;
