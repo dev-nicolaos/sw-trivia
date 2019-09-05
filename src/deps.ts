82ce943f8b2921948cb510949631ab290e398116
dev-nicolaos <contact@nicolaos.dev>
2019-09-05 02:01:20 -0700

    bump dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -5,5 +5,5 @@ export {
   bgYellow,
   black,
   white,
-} from 'https://deno.land/std@v0.16.0/fmt/colors.ts';
-export { parse } from 'https://deno.land/std@v0.16.0/flags/mod.ts';
+} from 'https://deno.land/std@v0.17.0/fmt/colors.ts';
+export { parse } from 'https://deno.land/std@v0.17.0/flags/mod.ts';
