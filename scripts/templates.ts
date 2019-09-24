c9b4262b2ccc426a02a80836021df5e4c4c4709b
dev-nicolaos <contact@nicolaos.dev>
2019-09-24 00:17:40 -0700

    prettier works better on temp. strings now

--- scripts/templates.ts
+++ scripts/templates.ts
@@ -3,9 +3,7 @@ import { SOURCE } from 'interfaces';
 export const generateBasicTriviaTemplate = (source: SOURCE) =>
   `import { TRIVIA, SOURCE } from 'interfaces';
 
-const source: SOURCE = { name: '${source.name}', mediaType: '${
-    source.mediaType
-  }' };
+const source: SOURCE = { name: '${source.name}', mediaType: '${source.mediaType}' };
 
 const trivia: TRIVIA[] = [];
 
