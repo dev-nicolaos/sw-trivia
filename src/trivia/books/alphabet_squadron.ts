764fefdf76292db8dc06cda5e7966b2faabfce39
dev-nicolaos <contact@nicolaos.dev>
2019-09-23 23:36:10 -0700

    add trivia for alphabet squadron novel

--- src/trivia/books/alphabet_squadron.ts
+++ src/trivia/books/alphabet_squadron.ts
@@ -2,6 +2,29 @@ import { TRIVIA, SOURCE } from 'interfaces';
 
 const source: SOURCE = { name: 'Alphabet Squadron', mediaType: 'Book' };
 
-const trivia: TRIVIA[] = [];
+const trivia: TRIVIA[] = [
+  {
+    question: 'Who convinced Yrica Quell to defect from the Empire?',
+    correctAnswer: 'Soran Keize',
+    wrongAnswers: ['Caern Adan', 'Jyn Erso', 'Iden Versio'],
+    source,
+  },
+  {
+    question: "Where did the Hellion's Dare attempt to escape Shadow Wing?",
+    correctAnswer: 'The Oridol Cluster',
+    wrongAnswers: [
+      'The Minos Cluster',
+      'The Kaliida Nebula',
+      'The Vulpinus Nebula',
+    ],
+    source,
+  },
+  {
+    question: 'Which starfighter was NOT a part of Alphabet Squadron?',
+    correctAnswer: 'V-wing',
+    wrongAnswers: ['A-wing', 'U-wing', 'X-wing'],
+    source,
+  },
+];
 
 export default trivia;
