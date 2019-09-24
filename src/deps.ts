24be9274bd9d873b93d0841fc4db1c0732357c7b
dev-nicolaos <contact@nicolaos.dev>
2019-09-24 00:17:01 -0700

    update to deno 0.18

--- src/deps.ts
+++ src/deps.ts
@@ -5,5 +5,5 @@ export {
   bgYellow,
   black,
   white,
-} from 'https://deno.land/std@v0.17.0/fmt/colors.ts';
-export { parse } from 'https://deno.land/std@v0.17.0/flags/mod.ts';
+} from 'https://deno.land/std@v0.18.0/fmt/colors.ts';
+export { parse } from 'https://deno.land/std@v0.18.0/flags/mod.ts';
--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 const VERSION = '0.1.0';
-const DENO_VERSION = '0.17.0';
+const DENO_VERSION = '0.18.0';
 
 export function printVersion() {
   console.log('Star Wars Trivia:', VERSION);
