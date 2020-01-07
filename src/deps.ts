a8a944b092755fe16c16f9c1acbe4a104b68a4e2
dev-nicolaos <contact@nicolaos.dev>
2020-01-07 08:23:47 -0800

    chore(deps): update dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -5,5 +5,5 @@ export {
   bgYellow,
   black,
   white,
-} from "https://deno.land/std@v0.27.0/fmt/colors.ts";
-export { parse } from "https://deno.land/std@v0.27.0/flags/mod.ts";
+} from "https://deno.land/std@v0.28.0/fmt/colors.ts";
+export { parse } from "https://deno.land/std@v0.28.0/flags/mod.ts";
