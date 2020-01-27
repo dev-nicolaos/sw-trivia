82759cea69783e3bb49cf0f43c5805b13e041ab8
dev-nicolaos <contact@nicolaos.dev>
2020-01-26 21:28:12 -0800

    feat: improve answer sorting

--- src/helpers/combine_answers.ts
+++ src/helpers/combine_answers.ts
@@ -1,11 +1,29 @@
 import { TRIVIA } from "interfaces";
 
+const isNumeric = testValue => Number.isInteger(+testValue);
+
+function shuffle(arr: any[]): any[] {
+  let currentIndex: number = arr.length;
+  let tempValue;
+  let randomIndex: number;
+
+  while (currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex--);
+
+    tempValue = arr[currentIndex];
+    arr[currentIndex] = arr[randomIndex];
+    arr[randomIndex] = tempValue;
+  }
+
+  return arr;
+}
+
 export function combineAnswers({
   wrongAnswers,
   correctAnswer,
 }: TRIVIA): string[] {
-  const combinedAnswers = [...wrongAnswers];
-  const insertIndex = Math.floor(Math.random() * (combinedAnswers.length + 1));
-  combinedAnswers.splice(insertIndex, 0, correctAnswer);
-  return combinedAnswers;
+  const combinedAnswers = [...wrongAnswers, correctAnswer];
+  return combinedAnswers.every(isNumeric)
+    ? combinedAnswers.sort((a, b) => +a - +b)
+    : shuffle(combinedAnswers);
 }
