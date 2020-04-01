f423ca43c6a6b4ea7187f7af824129c5b74cf8ff
dev-nicolaos <contact@nicolaos.dev>
2020-04-01 09:34:16 -0700

    Add trivia file for Vader Immortal

--- src/trivia/games/mod.ts
+++ src/trivia/games/mod.ts
@@ -2,7 +2,12 @@ import { TRIVIA } from "interfaces";
 
 import battlefront2 from "./battlefront_2.ts";
 import jediFallenOrder from "./jedi_fallen_order.ts";
+import vaderImmortal from "./vader_immortal.ts";
 
-const trivia: TRIVIA[] = [...battlefront2, ...jediFallenOrder];
+const trivia: TRIVIA[] = [
+  ...battlefront2,
+  ...jediFallenOrder,
+  ...vaderImmortal,
+];
 
 export default trivia;
---
+++ src/trivia/games/vader_immortal.ts
@@ -0,0 +1,7 @@
+import { TRIVIA, SOURCE } from "interfaces";
+
+const source: SOURCE = { name: "Vader Immortal", mediaType: "Game" };
+
+const trivia: TRIVIA[] = [];
+
+export default trivia;
