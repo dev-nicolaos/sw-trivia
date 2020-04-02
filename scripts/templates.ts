1b83f798e815891dd84beeac860d59660dd0e5e6
dev-nicolaos <contact@nicolaos.dev>
2020-04-01 22:07:47 -0700

    add TV and comic templates

--- scripts/templates.ts
+++ scripts/templates.ts
@@ -1,9 +1,31 @@
 import { SOURCE } from "interfaces";
 
-export const generateBasicTriviaTemplate = (source: SOURCE) =>
+export const generateBasicTriviaTemplate = ({ mediaType, name }: SOURCE) =>
   `import { TRIVIA, SOURCE } from "interfaces";
 
-const source: SOURCE = { name: "${source.name}", mediaType: "${source.mediaType}" };
+const source: SOURCE = { name: "${name}", mediaType: "${mediaType}" };
+
+const trivia: TRIVIA[] = [];
+
+export default trivia;
+`;
+
+export const generateTVTemplate = (seriesName: string) =>
+  `import { TRIVIA } from "interfaces";
+import { generateTVSource } from "helpers";
+
+const genSource = (episode: string) => generateTVSource("${seriesName}", episode);
+
+const trivia: TRIVIA[] = [];
+
+export default trivia;
+`;
+
+export const generateComicTemplate = (seriesName: string) =>
+  `import { TRIVIA } from "interfaces";
+import { generateComicSource } from "helpers";
+
+const genSource = (issue: number) => generateComicSource("${seriesName}", issue);
 
 const trivia: TRIVIA[] = [];
 
