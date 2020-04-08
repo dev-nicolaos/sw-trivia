d24f9bf41173b6144cac44e6dfab36789ee3f3c1
dev-nicolaos <contact@nicolaos.dev>
2020-04-08 07:32:49 -0700

    add questions for resistance reborn

--- src/trivia/books/resistance_reborn.ts
+++ src/trivia/books/resistance_reborn.ts
@@ -2,6 +2,25 @@ import { TRIVIA, SOURCE } from "types";
 
 const source: SOURCE = { name: "Resistance Reborn", mediaType: "Book" };
 
-const trivia: TRIVIA[] = [];
+const trivia: TRIVIA[] = [
+  {
+    question: "Where did the Resistance seek refuge after the Battle of Crait?",
+    correctAnswer: "Ryloth",
+    wrongAnswers: ["Hoth", "Corellia", "D'Qar"],
+    source,
+  },
+  {
+    question: "Who did Wedge Antilles marry?",
+    correctAnswer: "Norra Wexely",
+    wrongAnswers: ["Hera Syndulla", "Mon Mothma", "Iden Versio"],
+    source,
+  },
+  {
+    question: 'Which group\'s slogan was "All Knowledge Must be Free"?',
+    correctAnswer: "The Collective",
+    wrongAnswers: ["The GenoHaradan", "The Order of Revan", "The Droid Gotra"],
+    source,
+  },
+];
 
 export default trivia;
