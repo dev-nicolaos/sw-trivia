269919fc577a08da39aea49ca420efc1bdfbaa26
dev-nicolaos <contact@nicolaos.dev>
2020-04-11 11:23:59 -0700

    bump deps (deno and deno std) to v0.40.0

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,7 @@
 import { getYesNoResponse } from "helpers";
 
 const SW_TRIVIA_VERSION = "0.7.0";
-const TARGET_DENO_VERSION = "0.39.0";
+const TARGET_DENO_VERSION = "0.40.0";
 
 export function printVersion(): void {
   console.log("Star Wars Trivia:", SW_TRIVIA_VERSION);
