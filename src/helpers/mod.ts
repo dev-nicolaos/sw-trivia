813684a956cacf95381f38471816ee4f13ba50af
dev-nicolaos <contact@nicolaos.dev>
2020-04-14 23:40:01 -0700

    refactor bundle files

--- src/helpers/mod.ts
+++ src/helpers/mod.ts
@@ -1,21 +1,7 @@
-export {
-  askMultipleTrivia,
-  askRandomTriviaQuestion,
-  askTriviaQuestion,
-} from "./ask_trivia.ts";
-export { combineAnswers } from "./combine_answers.ts";
-export { generateComicSource, generateTVSource } from "./generate_source.ts";
-export {
-  getNumericInput,
-  getUserInput,
-  getYesNoResponse,
-} from "./get_user_input.ts";
-export {
-  printNegative,
-  printNeutral,
-  printPositive,
-  printQuestion,
-  printScore,
-} from "./logger.ts";
-export { pickRandomTrivia, pickTrivia } from "./pick_trivia.ts";
-export { startQuiz } from "./start_quiz.ts";
+export * from "./ask_trivia.ts";
+export * from "./combine_answers.ts";
+export * from "./generate_source.ts";
+export * from "./get_user_input.ts";
+export * from "./logger.ts";
+export * from "./pick_trivia.ts";
+export * from "./start_quiz.ts";
--- src/stats/mod.ts
+++ src/stats/mod.ts
@@ -1,6 +1,3 @@
-export { countUniqueSources } from "./count_unique_sources.ts";
-export {
-  findMostCommonSources,
-  formatMostCommonSources,
-} from "./most_common_sources.ts";
-export { printStats } from "./print_stats.ts";
+export * from "./count_unique_sources.ts";
+export * from "./most_common_sources.ts";
+export * from "./print_stats.ts";
