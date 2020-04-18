135e9fbc8b84e4be35509e2029e582a0411faff2
dev-nicolaos <contact@nicolaos.dev>
2020-04-18 14:59:19 -0700

    add file for resistance season 2

--- src/trivia/television/resistance/mod.ts
+++ src/trivia/television/resistance/mod.ts
@@ -1,7 +1,8 @@
 import { TRIVIA } from "types";
 
 import resistanceSeason1 from "./season_1.ts";
+import resistanceSeason2 from "./season_2.ts";
 
-const trivia: TRIVIA[] = [...resistanceSeason1];
+const trivia: TRIVIA[] = [...resistanceSeason1, ...resistanceSeason2];
 
 export default trivia;
---
+++ src/trivia/television/resistance/season_2.ts
@@ -0,0 +1,8 @@
+import { TRIVIA } from "types";
+import { generateTVSource } from "helpers";
+
+const genSource = (episode: string) => generateTVSource("Resistance", episode);
+
+const trivia: TRIVIA[] = [];
+
+export default trivia;
