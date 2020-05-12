fa6bf1919d7338c819195ebc1ff502660ef2d006
dev-nicolaos <contact@nicolaos.dev>
2020-05-12 08:38:58 -0700

    add more trivia for thrawn treason

--- src/trivia/books/thrawn_treason.ts
+++ src/trivia/books/thrawn_treason.ts
@@ -16,6 +16,26 @@ const trivia: TRIVIA[] = [
     wrongAnswers: ["Mynocks", "Purrgils", "Neebrays"],
     source,
   },
+  {
+    question: 'What ability did the Chiss refer to as "Second Sight"?',
+    correctAnswer: "Telepathy",
+    wrongAnswers: ["Precognition", "Telekinesis", "Battle meditation"],
+    source,
+  },
+  {
+    question:
+      "What type of fighters did Thrawn loan Admiral Ar'alani to fight the Grysks?",
+    correctAnswer: "TIE Defenders",
+    wrongAnswers: ["TIE Fighters", "TIE Interceptors", "TIE Bombers"],
+    source,
+  },
+  {
+    question:
+      "Who was behind the operation stealing turbolaser parts from Project Stardust?",
+    correctAnswer: "Grand Admiral Savit",
+    wrongAnswers: ["Grand Moff Tarkin", "Govenor Haveland", " Colonel Yularen"],
+    source,
+  },
 ];
 
 export default trivia;
