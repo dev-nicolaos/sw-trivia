fefe1608fb3057c36b6a93bafd50f6ad5b5d609a
dev-nicolaos <contact@nicolaos.dev>
2020-05-28 17:13:42 -0700

    add solo trivia

--- src/trivia/films/solo.ts
+++ src/trivia/films/solo.ts
@@ -4,6 +4,19 @@ const source: SOURCE = { name: "Solo", mediaType: "Film" };
 
 const trivia: TRIVIA[] = [
   {
+    question: "What gang were Han and Qi'ra part of on Corellia?",
+    correctAnswer: "White Worms",
+    wrongAnswers: ["Crimson Dawn", "Guavian Death Gang", "Shadow Collective"],
+    source,
+  },
+  {
+    question:
+      "What did Han use to bribe an Imperial officer while escaping from Corellia?",
+    correctAnswer: "Coaxium",
+    wrongAnswers: ["Corellian hyperfuel", "Imperial credits", "His ship"],
+    source,
+  },
+  {
     question: "Who killed Aurra Sing?",
     correctAnswer: "Tobias Beckett",
     wrongAnswers: ["Boba Fett", "Lando Calrissian", "Darth Maul"],
