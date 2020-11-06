fa011833f53ba697a881ed6fd40a6cf1e503a5fb
dev-nicolaos <contact@nicolaos.dev>
2020-11-05 19:36:18 -0800

    feat(content): add ESB trivia

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,7 +8,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/).
 
 ### Added
 
-- Trivia from _Return of the Jedi_
+- Trivia from _The Empire Strikes Back_ and _Return of the Jedi_
 
 ### Changed
 
--- src/trivia/films/the_empire_strikes_back.ts
+++ src/trivia/films/the_empire_strikes_back.ts
@@ -117,6 +117,180 @@ const trivia: TRIVIA[] = [
     ],
     source,
   },
+  {
+    question:
+      "Why did Han want C-3PO to talk to the Millennium Falcon in the asteroid field?",
+    correctAnswer: "To find out what was wrong with the hyperdrive",
+    wrongAnswers: [
+      "To find out what was wrong with the navicomputer",
+      "To chart a path through the asteroid field",
+      "To send a distress signal",
+    ],
+    source,
+  },
+  {
+    question:
+      "What did R2-D2 try to take from Yoda after Luke landed on Dagobah for the first time?",
+    correctAnswer: "Luke's lamp",
+    wrongAnswers: ["Luke's food", "R2-D2's power supply", "Yoda's cane"],
+    source,
+  },
+  {
+    question: "Who interupted Han and Leia's first kiss?",
+    correctAnswer: "C-3PO",
+    wrongAnswers: ["Chewbacca", "R2-D2", "A stormtrooper"],
+    source,
+  },
+  {
+    question:
+      "Why did Vader order the Executor moved out of the asteroid field?",
+    correctAnswer: "To be able to send a clear transmission to the Emperor",
+    wrongAnswers: [
+      "To prevent it from taking any more damage",
+      "To dump the garbage on board",
+      "To retrieve several TIE squadrons",
+    ],
+    source,
+  },
+  {
+    question: "What reason did Luke give Yoda for wanting to become a Jedi?",
+    correctAnswer: "Because of father",
+    wrongAnswers: [
+      "To defeat evil",
+      "To save his friends",
+      "Because of Obi-Wan",
+    ],
+    source,
+  },
+  {
+    question: "What species stuck to Millenium Falcon inside the space slug?",
+    correctAnswer: "Mynocks",
+    wrongAnswers: ["Purrgil", "Grallocs", "Neebray"],
+    source,
+  },
+  {
+    question:
+      "What did Yoda advise Luke not to take into the dark side cave on Dagobah",
+    correctAnswer: "His weapons",
+    wrongAnswers: ["His jacket", "R2-D2", "Anger"],
+    source,
+  },
+  {
+    question:
+      "Which bounty hunter was NOT recruited by the Empire to find the Millenium Falcon?",
+    correctAnswer: "IG-11",
+    wrongAnswers: ["Boba Fett", "Bossk", "Dengar"],
+    source,
+  },
+  {
+    question:
+      "Which Star Destroyer chased the Millenium Falcon after it fled the space slug?",
+    correctAnswer: "Avenger",
+    wrongAnswers: ["Devastator", "Torment", "Overseer"],
+    source,
+  },
+  {
+    question: 'Finish the quote: "Judge me by my _____, do you?"',
+    correctAnswer: "size",
+    wrongAnswers: ["power", "age", "wealth"],
+    source,
+  },
+  {
+    question: "Where did Luke see Han and Leia in his vision on Dagobah?",
+    correctAnswer: "A city in the clouds",
+    wrongAnswers: ["An Imperial prison", "A barren desert", "A frozen tundra"],
+    source,
+  },
+  {
+    question: "What did Lando call Han when he landed on Cloud City?",
+    correctAnswer: "A swindler",
+    wrongAnswers: ["A thief", "Rebel scum", "Broke"],
+    source,
+  },
+  {
+    question: "Where did Chewbacca find C-3PO on Cloud City?",
+    correctAnswer: "A junk pile",
+    wrongAnswers: [
+      "The Administrator's Palace",
+      "Carbon Freezing Chamber",
+      "Yarith Bespin",
+    ],
+    source,
+  },
+  {
+    question:
+      "How many shots did Han fire at Darth Vader when Lando betrayed the Rebels on Cloud City?",
+    correctAnswer: "4",
+    wrongAnswers: ["2", "3", "5"],
+    source,
+  },
+  {
+    question:
+      "What did Vader question Han about while torturing him on Cloud City?",
+    correctAnswer: "Nothing",
+    wrongAnswers: ["The Rebel Alliance", "His debts", "Lando Calrissian"],
+    source,
+  },
+  {
+    question:
+      "What did Darth Vader promise to do if Han Solo did not survive the carbon-freezing process?",
+    correctAnswer: "Compensate Boba Fett",
+    wrongAnswers: [
+      "Kill Lando Calrissian",
+      "Take Luke to the Emperor",
+      "Demote Admiral Piett",
+    ],
+    source,
+  },
+  {
+    question: "Who warned Luke he was walking into a trap on Cloud City?",
+    correctAnswer: "Leia Organa",
+    wrongAnswers: ["Lando Calrissian", "An Imperial defector", "R2-D2"],
+    source,
+  },
+  {
+    question:
+      "Who did Lando instruct to hold captured stormtroopers in the security tower on Cloud City?",
+    correctAnswer: "Lobot",
+    wrongAnswers: ["Chewbacca", "Boba Fett", "Sana Starros"],
+    source,
+  },
+  {
+    question:
+      "What did R2-D2 accidentally plug into while trying to override Cloud City's security systems?",
+    correctAnswer: "A power socket",
+    wrongAnswers: [
+      "An elevator",
+      "A lifeform scanner",
+      "A communications satelite",
+    ],
+    source,
+  },
+  {
+    question:
+      "What body part(s) did Luke lose while dueling Darth Vader on Cloud City?",
+    correctAnswer: "Right hand",
+    wrongAnswers: ["Left hand", "Both Legs", "Left arm"],
+    source,
+  },
+  {
+    question:
+      "Who helped Luke through the top hatch of the Millenium Falcon underneath Cloud City?",
+    correctAnswer: "Lando Calrissian",
+    wrongAnswers: ["Chewbacca", "Leia Organa", "Obi-Wan Kenobi"],
+    source,
+  },
+  {
+    question:
+      "Why wasn't the Millenium Falcon able to jump to lightspeed when leaving Cloud City?",
+    correctAnswer: "Imperials had disabled the hyperdrive",
+    wrongAnswers: [
+      "Lando entered incorrect jump coordinates",
+      "The hyperdrive had been damaged by TIE fighters",
+      "It was out of coaxium",
+    ],
+    source,
+  },
 ];
 
 export default trivia;
